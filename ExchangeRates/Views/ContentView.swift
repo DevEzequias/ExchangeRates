427f838b50f123bada6b74724f2ca43dbc099449
devezequias <ezequiasantos77@hotmail.com>
2023-10-19 16:51:11 -0400

    Adicionando listage de flutuacao de moedas

---
+++ ExchangeRates/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ExchangeRates
+//
+//  Created by Ezequias Santos on 18/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        RatesFluctuationView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
